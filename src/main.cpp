8ca5f59d6a5241eb3439bc2d00ccf063b897f4cc
yanghai <yanghai@xunlei.com>
2024-09-14 18:16:43 +0800

    my engine

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,98 @@
 #include <SFML/Graphics.hpp>
 
+class Game
+{
+  public:
+    Game();
+    void run();
+
+  private:
+    sf::RenderWindow mWindow;
+    sf::CircleShape mPlayer;
+    bool mIsMovingUp;
+    bool mIsMovingDown;
+    bool mIsMovingLeft;
+    bool mIsMovingRight;
+
+    void processEvents();
+    void update();
+    void render();
+    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+};
+
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    Game game;
+    game.run();
+}
 
-    while (window.isOpen())
+Game::Game() : mWindow(sf::VideoMode(640, 482), "SFML TEST")
+{
+    mPlayer.setRadius(40.f);
+    mPlayer.setPosition(100.f, 100.f);
+    mPlayer.setFillColor(sf::Color::Cyan);
+}
+
+void Game::run()
+{
+    while (mWindow.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
+        processEvents();
+        update();
+        render();
+    }
+}
 
-        window.clear();
-        window.display();
+void Game::processEvents()
+{
+    sf::Event event{};
+    while (mWindow.pollEvent(event))
+    {
+        switch (event.type)
+        {
+        case sf::Event::KeyPressed:
+            handlePlayerInput(event.key.code, true);
+            break;
+        case sf::Event::KeyReleased:
+            handlePlayerInput(event.key.code, false);
+            break;
+        case sf::Event::Closed:
+            mWindow.close();
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void Game::update()
+{
+}
+
+void Game::render()
+{
+    mWindow.clear();
+    mWindow.draw(mPlayer);
+    mWindow.display();
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+    switch (key)
+    {
+    case sf::Keyboard::Key::W:
+        mIsMovingUp = isPressed;
+        break;
+    case sf::Keyboard::Key::S:
+        mIsMovingDown = isPressed;
+        break;
+    case sf::Keyboard::Key::A:
+        mIsMovingLeft = isPressed;
+        break;
+    case sf::Keyboard::Key::D:
+        mIsMovingRight = isPressed;
+        break;
+    default:
+        break;
     }
 }
